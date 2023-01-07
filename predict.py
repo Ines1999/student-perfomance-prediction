bfe1e8297bc8eb84ab35a202f2575c3271e6532f
ines <ines-99@ukr.net>
2023-01-07 21:31:10 +0200

    add improvement to knn performance and choose it for prediction

--- predict.py
+++ predict.py
@@ -5,15 +5,14 @@ from sklearn import model_selection
 
 
 def function(argv):
-	input = argv[1]
-	data = pd.read_excel(input)
-	loaded_model = joblib.load('./Models/decision_tree.pkl')
-	result = loaded_model.predict(data)
+    input = argv[1]
+    data = pd.read_excel(input)
+    loaded_model = joblib.load('./Models/knn.pkl')
+    result = loaded_model.predict(data)
 
-	if int(result) == 0:
-		print("This student is most likely to fail the mathematics subject")
-	else:
-		print("This student is most likely to pass the mathematics subject")
+    if int(result) == 0:
+        print("OPS :(!This student is most likely to fail the mathematics subject")
+    else:
+        print("YAY!This student is most likely to pass the mathematics subject")
 
-if __name__ == '__main__':
-	function(sys.argv)
+function(sys.argv)
